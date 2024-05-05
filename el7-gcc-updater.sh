5792a49dcd9d2cdc539fefadf003a5a39ea1c2ff
abhpc <xyliucd@foxmail.com>
2024-05-05 09:23:43 +0800

    Download necessary package from ftp.gnu.org

--- el7-gcc-updater.sh
+++ el7-gcc-updater.sh
@@ -7,11 +7,12 @@ JN="20"
 wget $SOFT_SERV/gcc-7.5.0.tar.xz --no-check-certificate
 wget $SOFT_SERV/gcc-9.5.0.tar.xz --no-check-certificate
 wget $SOFT_SERV/gcc-13.1.0.tar.xz --no-check-certificate
-wget $SOFT_SERV/gmp-6.1.2.tar.xz --no-check-certificate
-wget $SOFT_SERV/mpfr-3.1.6.tar.gz --no-check-certificate
-wget $SOFT_SERV/mpc-1.0.3.tar.gz --no-check-certificate
+
+# Download gmp, mpfr and mpc
 
 # Install gmp 6.1.2
+rm -rf gmp-6.1.2.tar.xz
+wget https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.xz --no-check-certificate
 tar xvf gmp-6.1.2.tar.xz
 cd gmp-6.1.2
 ./configure --prefix=$DES/gmp-6.1.2
@@ -19,6 +20,8 @@ make -j $JN && make install
 cd ..
 
 # Install mpfr 3.1.6
+rm -rf mpfr-3.1.6.tar.gz
+wget https://ftp.gnu.org/gnu/mpfr/mpfr-3.1.6.tar.gz --no-check-certificate
 tar xvf mpfr-3.1.6.tar.gz
 cd mpfr-3.1.6
 ./configure --prefix=$DES/mpfr-3.1.6 --with-gmp=$DES/gmp-6.1.2
@@ -26,6 +29,8 @@ make -j $JN && make install
 cd ..
 
 # Install mpc 1.0.3
+rm -rf mpc-1.0.3.tar.gz
+wget https://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz --no-check-certificate
 tar xvf mpc-1.0.3.tar.gz
 cd mpc-1.0.3
 ./configure --prefix=$DES/mpc-1.0.3 --with-gmp=$DES/gmp-6.1.2 --with-mpfr=$DES/mpfr-3.1.6
